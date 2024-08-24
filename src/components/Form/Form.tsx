73278bc5a4607720eb1ea68a875312c7a50dc669
Sehee Yun <seheeyun29@gmail.com>
2024-08-25 03:53:49 +0900

    feat: Form 컴포넌트 구현

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
+import Button from '../Button';
+import styles from './Form.module.scss';
+
+interface Props {
+  data?: {
+    id: string;
+    title: string;
+    content: string;
+  };
+  onSubmit: (formData: FormData) => void;
+  onCancel: () => void;
+}
+
+export default function Form({ data, onCancel, onSubmit }: Props) {
+  const { pending } = useFormStatus();
+
+  return (
+    <form className={styles.form}>
+      {data?.id && <input type="hidden" name="id" value={data?.id} />}
+      <input
+        type="text"
+        name="title"
+        placeholder="제목을 입력해주세요"
+        required
+        autoFocus
+        className={`${styles.input}`}
+        defaultValue={data?.title}
+      />
+      <textarea
+        name="content"
+        placeholder="내용을 입력해주세요"
+        required
+        rows={10}
+        className={`${styles.input}`}
+        defaultValue={data?.content}
+      />
+      <div className={styles.button_wrapper}>
+        <Button onClick={onCancel}>취소</Button>
+        <Button
+          type="submit"
+          theme="primary"
+          disabled={pending}
+          formAction={onSubmit}
+        >
+          등록
+        </Button>
+      </div>
+    </form>
+  );
+}
