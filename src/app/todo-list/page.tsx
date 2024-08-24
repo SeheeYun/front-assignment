6c087bb5f3e00361ddf40f6a7c9265fd07f8683b
Sehee Yun <seheeyun29@gmail.com>
2024-08-25 03:57:47 +0900

    feat: todo-list 페이지 구현

---
+++ src/app/todo-list/page.tsx
@@ -0,0 +1,37 @@
+import styles from './page.module.scss';
+import Button from '@/components/Button';
+import { Todo } from '@/types/data';
+import TodoItem from '@/components/TodoItem';
+import AddFormDialog from '@/components/AddFormDialog';
+
+async function fetchTodos() {
+  const res = await fetch('http://localhost:3001/todos');
+  const todos: Todo[] = await res.json();
+  return todos;
+}
+
+export default async function TodoListPage() {
+  const todos = await fetchTodos();
+
+  return (
+    <main>
+      <ul className={styles.ul}>
+        {todos.map(todo => {
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
+      </ul>
+      <div className={styles.button_wrapper}>
+        <div className={styles.button}>
+          <AddFormDialog
+            trigger={
+              <Button size="large" theme="primary">
+                추가하기
+              </Button>
+            }
+            title="Todo 추가하기"
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
